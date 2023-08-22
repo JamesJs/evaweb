fc708e65ffe470f2ca73db7cae353f764a20bb57
Thiago Baldino <thiagobaldino@Thiagos-MacBook-Air.local>
2023-08-22 16:03:33 -0300

    ajuste do valor de litros para opção todos

--- src/api/dadosDestilacao.js
+++ src/api/dadosDestilacao.js
@@ -1,102 +1,158 @@
-import Axios from "axios"
+import Axios from "axios";
 import { URL } from "../env";
 
+async function deletaDestilacao(id) {
+  const _token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtYWMiOiJzZWlsYSIsInNlcmlhbCI6InNlaWxhIiwiaWF0IjoxNjQ4ODMwOTM1fQ.ZLKZ8eEXMgAH987MzG7fWS6nN3Gdj9eHMaNhf4i_enU";
+  const _url = URL + "/destilacao/" + id;
 
-async function deletaDestilacao(id){
-
-	const _token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtYWMiOiJzZWlsYSIsInNlcmlhbCI6InNlaWxhIiwiaWF0IjoxNjQ4ODMwOTM1fQ.ZLKZ8eEXMgAH987MzG7fWS6nN3Gdj9eHMaNhf4i_enU"
-	const _url = URL+"/destilacao/"+id;
-	
-	var axiosHeaders = { 'Content-Type' : 'application/json',
-		'Authorization' : 'Bearer ' + _token,
-		'Accept': '*/*' };
-	const response = await Axios.delete(_url,{headers:axiosHeaders});
-	if(response.status !== 200) throw new Error(response.data);
-	
-
+  var axiosHeaders = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + _token,
+    Accept: "*/*",
+  };
+  const response = await Axios.delete(_url, { headers: axiosHeaders });
+  if (response.status !== 200) throw new Error(response.data);
 }
 
-async function getDados(id){
-	const _token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtYWMiOiJzZWlsYSIsInNlcmlhbCI6InNlaWxhIiwiaWF0IjoxNjQ4ODMwOTM1fQ.ZLKZ8eEXMgAH987MzG7fWS6nN3Gdj9eHMaNhf4i_enU"
-	const _url = URL+'/destilacao/'+id+'/dadosDestilacao'
-	var axiosHeaders = { 'Content-Type' : 'application/json',
-		'Authorization' : 'Bearer ' + _token,
-		'Accept': '*/*' };
+async function getDados(id) {
+  const _token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtYWMiOiJzZWlsYSIsInNlcmlhbCI6InNlaWxhIiwiaWF0IjoxNjQ4ODMwOTM1fQ.ZLKZ8eEXMgAH987MzG7fWS6nN3Gdj9eHMaNhf4i_enU";
+  const _url = URL + "/destilacao/" + id + "/dadosDestilacao";
+  var axiosHeaders = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + _token,
+    Accept: "*/*",
+  };
 
-	var _listaCabeca = [];
-	let media_temp_cabeca = 0;
-	let media_gl_cabeca = 0;
-	let litros_cabeca = 0;
-	let media_temp_coracao = 0;
-	let media_gl_coracao = 0;
-	let litros_coracao = 0;
-	let media_temp_cauda = 0;
-	let media_gl_cauda = 0;
-	let litros_cauda = 0;
-	var _listaCoracao = [];
-	var _listaCauda = [];
-	var _pontosCabeca = [];
-	var _pontosCauda = [];
-	var _pontosCoracao = [];
-	const response = await Axios.get(_url,{
-		headers: axiosHeaders})
-	response.data.forEach((value)=>{
-		switch (value.parte) {
-			case 'cabeca':
-				_listaCabeca[_listaCabeca.length] = value
-				break;
-			case 'coracao':
-				_listaCoracao[_listaCoracao.length] = value
-				break;
-			case 'cauda':
-				_listaCauda[_listaCauda.length] = value
-				break;
-			default:
-				console.log("Não achado")
-		}
-	})
-	//------------------------------------------------------------------------------------------------------------------------------
-	_listaCabeca.forEach((value)=>{
-		media_gl_cabeca = media_gl_cabeca + value.gl;
-		media_temp_cabeca = media_temp_cabeca + value.temperatura
-		_pontosCabeca[_pontosCabeca.length] = {"GL" : value.gl, "Temp" : value.temperatura, "amt" : value.litros, "name" : new Date(value.data).toLocaleTimeString("pt-Br",{timeStyle:"short",timeZone:"UTC"}) }
-	})
+  var _listaCabeca = [];
+  let media_temp_cabeca = 0;
+  let media_gl_cabeca = 0;
+  let litros_cabeca = 0;
+  let media_temp_coracao = 0;
+  let media_gl_coracao = 0;
+  let litros_coracao = 0;
+  let media_temp_cauda = 0;
+  let media_gl_cauda = 0;
+  let litros_cauda = 0;
+  var _listaCoracao = [];
+  var _listaCauda = [];
+  var _pontosCabeca = [];
+  var _pontosCauda = [];
+  var _pontosCoracao = [];
+  const response = await Axios.get(_url, {
+    headers: axiosHeaders,
+  });
+  response.data.forEach((value) => {
+    switch (value.parte) {
+      case "cabeca":
+        _listaCabeca[_listaCabeca.length] = value;
+        break;
+      case "coracao":
+        _listaCoracao[_listaCoracao.length] = value;
+        break;
+      case "cauda":
+        _listaCauda[_listaCauda.length] = value;
+        break;
+      default:
+        console.log("Não achado");
+    }
+  });
+  //------------------------------------------------------------------------------------------------------------------------------
+  _listaCabeca.forEach((value) => {
+    media_gl_cabeca = media_gl_cabeca + value.gl;
+    media_temp_cabeca = media_temp_cabeca + value.temperatura;
+    _pontosCabeca[_pontosCabeca.length] = {
+      GL: value.gl,
+      Temp: value.temperatura,
+      amt: value.litros,
+      name: new Date(value.data).toLocaleTimeString("pt-Br", {
+        timeStyle: "short",
+        timeZone: "UTC",
+      }),
+    };
+  });
 
-	_listaCoracao.forEach((value)=>{
-		media_gl_coracao = media_gl_coracao + value.gl;
-		media_temp_coracao = media_temp_coracao + value.temperatura
-		_pontosCoracao[_pontosCoracao.length] = {"GL" : value.gl, "Temp" : value.temperatura, "amt" : value.litros, "name" : new Date(value.data).toLocaleTimeString("pt-Br",{timeStyle:"short",timeZone:"UTC"}) }
-	})
+  _listaCoracao.forEach((value) => {
+    media_gl_coracao = media_gl_coracao + value.gl;
+    media_temp_coracao = media_temp_coracao + value.temperatura;
+    _pontosCoracao[_pontosCoracao.length] = {
+      GL: value.gl,
+      Temp: value.temperatura,
+      amt: value.litros,
+      name: new Date(value.data).toLocaleTimeString("pt-Br", {
+        timeStyle: "short",
+        timeZone: "UTC",
+      }),
+    };
+  });
 
-	_listaCauda.forEach((value)=>{
-		media_gl_cauda = media_gl_cauda + value.gl;
-		media_temp_cauda = media_temp_cauda + value.temperatura
-		_pontosCauda[_pontosCauda.length] = {"GL" : value.gl, "Temp" : value.temperatura, "amt" : value.litros, "name" : new Date(value.data).toLocaleTimeString("pt-Br",{timeStyle:"short",timeZone:"UTC"}) }
-	})
+  _listaCauda.forEach((value) => {
+    media_gl_cauda = media_gl_cauda + value.gl;
+    media_temp_cauda = media_temp_cauda + value.temperatura;
+    _pontosCauda[_pontosCauda.length] = {
+      GL: value.gl,
+      Temp: value.temperatura,
+      amt: value.litros,
+      name: new Date(value.data).toLocaleTimeString("pt-Br", {
+        timeStyle: "short",
+        timeZone: "UTC",
+      }),
+    };
+  });
 
+  let sumLitros = 0;
+  if (_pontosCoracao.length > 0)
+    sumLitros = sumLitros + _pontosCoracao[_pontosCoracao.length - 1]["amt"];
+  if (_pontosCabeca.length > 0)
+    sumLitros = sumLitros + _pontosCabeca[_pontosCabeca.length - 1]["amt"];
+  if (_pontosCauda.length > 0)
+    sumLitros = sumLitros + _pontosCauda[_pontosCauda.length - 1]["amt"];
 
-	let sumLitros = 0
-	if(_pontosCoracao.length > 0) sumLitros = sumLitros + _pontosCoracao[_pontosCoracao.length - 1]["atm"]
-	if(_pontosCabeca.length > 0) sumLitros = sumLitros + _pontosCabeca[_pontosCabeca.length - 1]["atm"]
-	if(_pontosCauda.length > 0) sumLitros = sumLitros + _pontosCauda[_pontosCauda.length - 1]["atm"]
-
-	return {
-		mediaTodos:{
-			temp:((media_temp_coracao+media_temp_cauda+media_temp_cabeca)/(_pontosCoracao.length+_pontosCauda.length+_pontosCabeca.length) || 0).toFixed(2), gl:((media_gl_coracao+media_gl_cauda+media_gl_cabeca)/(_pontosCauda.length+_pontosCabeca.length+_pontosCoracao.length) || 0).toFixed(2), litros:sumLitros},
-		mediaCabeca:{
-			temp:((media_temp_cabeca/_pontosCabeca.length) || 0).toFixed(2), gl:((media_gl_cabeca/_pontosCabeca.length) || 0).toFixed(2),litros:(_pontosCabeca.length > 0 ? _pontosCabeca[_pontosCabeca.length - 1]["amt"]: 0).toFixed(2)
-		},
-		mediaCauda:{
-			temp:((media_temp_cauda/_pontosCauda.length) || 0).toFixed(2), gl:((media_gl_cauda/_pontosCauda.length) || 0).toFixed(2)      ,litros:(_pontosCauda.length > 0 ? _pontosCauda[_pontosCauda.length - 1]["amt"] : 0).toFixed(2)
-		},
-		mediaCoracao:{
-
-			temp:((media_temp_coracao/_pontosCoracao.length) || 0).toFixed(2), gl:((media_gl_coracao/_pontosCoracao.length) || 0).toFixed(2),litros:(_pontosCoracao.length > 0 ? _pontosCoracao[_pontosCoracao.length-1]["amt"] : 0).toFixed(2)
-		},
-		_pontosCabeca,
-		_pontosCoracao,
-		_pontosCauda
-	}
+  return {
+    mediaTodos: {
+      temp: (
+        (media_temp_coracao + media_temp_cauda + media_temp_cabeca) /
+          (_pontosCoracao.length +
+            _pontosCauda.length +
+            _pontosCabeca.length) || 0
+      ).toFixed(2),
+      gl: (
+        (media_gl_coracao + media_gl_cauda + media_gl_cabeca) /
+          (_pontosCauda.length +
+            _pontosCabeca.length +
+            _pontosCoracao.length) || 0
+      ).toFixed(2),
+      litros: sumLitros,
+    },
+    mediaCabeca: {
+      temp: (media_temp_cabeca / _pontosCabeca.length || 0).toFixed(2),
+      gl: (media_gl_cabeca / _pontosCabeca.length || 0).toFixed(2),
+      litros: (_pontosCabeca.length > 0
+        ? _pontosCabeca[_pontosCabeca.length - 1]["amt"]
+        : 0
+      ).toFixed(2),
+    },
+    mediaCauda: {
+      temp: (media_temp_cauda / _pontosCauda.length || 0).toFixed(2),
+      gl: (media_gl_cauda / _pontosCauda.length || 0).toFixed(2),
+      litros: (_pontosCauda.length > 0
+        ? _pontosCauda[_pontosCauda.length - 1]["amt"]
+        : 0
+      ).toFixed(2),
+    },
+    mediaCoracao: {
+      temp: (media_temp_coracao / _pontosCoracao.length || 0).toFixed(2),
+      gl: (media_gl_coracao / _pontosCoracao.length || 0).toFixed(2),
+      litros: (_pontosCoracao.length > 0
+        ? _pontosCoracao[_pontosCoracao.length - 1]["amt"]
+        : 0
+      ).toFixed(2),
+    },
+    _pontosCabeca,
+    _pontosCoracao,
+    _pontosCauda,
+  };
 }
 
-export {getDados,deletaDestilacao}
+export { getDados, deletaDestilacao };
--- src/pages/Chart/index.tsx
+++ src/pages/Chart/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Paper, Switch } from "@mui/material";
-import React,{useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import TwoSameArea from "../../components/Chart";
 import TwoCharts from "../../components/TwoChart";
 import { ButtonBase } from "@mui/material";
@@ -8,129 +8,124 @@ import ArrowFoward from "@mui/icons-material/ArrowForward";
 import RadioButton from "../../components/RadioButton";
 import BotoesOp from "../../components/BotoesOp";
 import "./styles.css";
-import {getDados} from "../../api/dadosDestilacao"
+import { getDados } from "../../api/dadosDestilacao";
 export default function Chart() {
-	const [data,setData] = useState<any>([{}])
-	const [dateString,setDateString] = useState<string>('')
-	const dados = useRef<Record<string,any>>({})
-	const [media_temp,setMedia_temp] = useState<string>()
-	const [media_gl,setMedia_gl] = useState<string>()
-	const [media_litros,setMedia_litros] = useState<string>()
-	const id = useRef<number|undefined|null>();
-	useEffect(() => {
-		const params = new URLSearchParams(window.location.search);
-		id.current = params.get("id") !== null ? Number(params.get("id")):-1;
-		async function getDadosAsync(){
-			dados.current =  await getDados(id.current);
-			setData(dados.current._pontosCabeca);
-			setMedia_gl(dados.current.mediaCabeca.gl);
-			setMedia_temp(dados.current.mediaCabeca.temp);
-			setMedia_litros(dados.current.mediaCabeca.litros)
-		}
-		setDateString(params.get("data")||'');
-		getDadosAsync();
-	}, []);
-	const handlerChangePart = function(part:string){
-		if(part === "cabeca"){
-			setMedia_gl(dados.current.mediaCabeca.gl);
-			setMedia_temp(dados.current.mediaCabeca.temp);
-			setMedia_litros(dados.current.mediaCabeca.litros)
-			setData(dados.current._pontosCabeca)
-		}else if(part === "coracao"){
+  const [data, setData] = useState<any>([{}]);
+  const [dateString, setDateString] = useState<string>("");
+  const dados = useRef<Record<string, any>>({});
+  const [media_temp, setMedia_temp] = useState<string>();
+  const [media_gl, setMedia_gl] = useState<string>();
+  const [media_litros, setMedia_litros] = useState<string>();
+  const id = useRef<number | undefined | null>();
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    id.current = params.get("id") !== null ? Number(params.get("id")) : -1;
+    async function getDadosAsync() {
+      dados.current = await getDados(id.current);
+      setData(dados.current._pontosCabeca);
+      setMedia_gl(dados.current.mediaCabeca.gl);
+      setMedia_temp(dados.current.mediaCabeca.temp);
+      setMedia_litros(dados.current.mediaCabeca.litros);
+    }
+    setDateString(params.get("data") || "");
+    getDadosAsync();
+  }, []);
+  const handlerChangePart = function (part: string) {
+    if (part === "cabeca") {
+      setMedia_gl(dados.current.mediaCabeca.gl);
+      setMedia_temp(dados.current.mediaCabeca.temp);
+      setMedia_litros(dados.current.mediaCabeca.litros);
+      setData(dados.current._pontosCabeca);
+    } else if (part === "coracao") {
+      setMedia_gl(dados.current.mediaCoracao.gl);
+      setMedia_temp(dados.current.mediaCoracao.temp);
+      setMedia_litros(dados.current.mediaCoracao.litros);
+      setData(dados.current._pontosCoracao);
+    } else if (part === "cauda") {
+      setMedia_gl(dados.current.mediaCauda.gl);
+      setMedia_temp(dados.current.mediaCauda.temp);
+      setMedia_litros(dados.current.mediaCauda.litros);
+      setData(dados.current._pontosCauda);
+    } else {
+      console.log(dados.current.mediaTodos);
+      setMedia_gl(dados.current.mediaTodos.gl);
+      setMedia_temp(dados.current.mediaTodos.temp);
+      setMedia_litros(dados.current.mediaTodos.litros);
 
-			setMedia_gl(dados.current.mediaCoracao.gl);
-			setMedia_temp(dados.current.mediaCoracao.temp);
-			setMedia_litros(dados.current.mediaCoracao.litros)
-			setData(dados.current._pontosCoracao)
-		}else if(part === "cauda"){
+      setData([
+        ...dados.current._pontosCabeca,
+        ...dados.current._pontosCauda,
+        ...dados.current._pontosCoracao,
+      ]);
+    }
+  };
+  const [checked, setChecked] = React.useState(false);
 
-			setMedia_gl(dados.current.mediaCauda.gl);
-			setMedia_temp(dados.current.mediaCauda.temp);
-			setMedia_litros(dados.current.mediaCauda.litros)
-			setData(dados.current._pontosCauda)
-		}else{
-			setMedia_gl(dados.current.mediaTodos.gl);
-			setMedia_temp(dados.current.mediaTodos.temp);
-			setMedia_litros(dados.current.mediaTodos.litros)
+  const [expandir, setExpandir] = React.useState(false);
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Box className="title-charts-page">
+        <h1>Destilação do dia: {dateString}</h1>
+      </Box>
 
-			setData([...dados.current._pontosCabeca,...dados.current._pontosCauda,...dados.current._pontosCoracao])
-		}
+      <Box sx={{ display: "flex", marginBottom: 10 }}>
+        <Paper sx={{ marginRight: 10, padding: 5 }}>
+          <h3>Valor médio da temperatura: {media_temp} ºC</h3>
+        </Paper>
+        <Paper sx={{ padding: 5, marginRight: 10 }}>
+          <h3>Valor médio do ºGL:{media_gl}</h3>
+        </Paper>
+        <Paper sx={{ padding: 5 }}>
+          <h3>Litros totais: {media_litros} L</h3>
+        </Paper>
+      </Box>
 
-	}
-	const [checked, setChecked] = React.useState(false);
+      <Box className="chart-warp">
+        {checked ? (
+          <TwoCharts compacto={!expandir} data={data} />
+        ) : (
+          <TwoSameArea compacto={!expandir} data={data} />
+        )}
+      </Box>
+      <Box className="options-warp-chart">
+        <Box
+          sx={{
+            display: "flex",
+          }}
+        >
+          <RadioButton handlerChange={handlerChangePart} />
+          <div className="options-buttons-chart">
+            <BotoesOp data={dateString} id={id.current} />
+          </div>
+        </Box>
+        <Box className="switch-charts-warp">
+          <b className="switch-charts-text">Dividir em dois gráficos</b>
+          <Switch
+            onClick={() => {
+              setChecked(!checked);
+            }}
+            checked={checked}
+            size="medium"
+          />
+        </Box>
 
-	const [expandir, setExpandir] = React.useState(false);
-	return (
-		<Box
-			sx={{
-				display: "flex",
-				flexDirection: "column",
-			}}
-		>
-			<Box
-				className="title-charts-page"
-			>
-				<h1>Destilação do dia: {dateString}</h1>
-			</Box>
-
-			<Box sx={{ display: "flex", marginBottom: 10 }}>
-				<Paper sx={{ marginRight: 10, padding: 5 }}>
-					<h3>Valor médio da temperatura: {media_temp} ºC</h3>
-				</Paper>
-				<Paper sx={{ padding: 5, marginRight: 10 }}>
-					<h3>Valor médio do ºGL:{media_gl}</h3>
-				</Paper>
-				<Paper sx={{ padding: 5 }}>
-					<h3>Litros totais: {media_litros} L</h3>
-				</Paper>
-			</Box>
-
-			<Box className="chart-warp">
-				{checked ? <TwoCharts compacto={!expandir} data={data} /> : <TwoSameArea compacto={!expandir} data={data}/>}
-			</Box>
-			<Box
-				className="options-warp-chart"
-			>
-				<Box
-					sx={{
-						display: "flex",
-					}}
-				>
-					<RadioButton handlerChange={handlerChangePart} />
-					<div className="options-buttons-chart">
-						<BotoesOp data={dateString} id={id.current} />
-					</div>
-				</Box>
-				<Box
-					className="switch-charts-warp"
-				>
-					<b className="switch-charts-text">Dividir em dois gráficos</b>
-					<Switch
-						onClick={() => {
-							setChecked(!checked);
-						}}
-						checked={checked}
-						size="medium"
-					/>
-				</Box>
-
-				<Box
-					className="switch-charts-warp"
-				>
-					<b className="switch-charts-text">Expandir gráfico</b>
-					<Switch
-						onClick={() => {
-							setExpandir(!expandir);
-						}}
-						checked={expandir}
-						size="medium"
-					/>
-				</Box>
-				<Box
-					className="options-pagination"
-				>
-					{
-						/*
+        <Box className="switch-charts-warp">
+          <b className="switch-charts-text">Expandir gráfico</b>
+          <Switch
+            onClick={() => {
+              setExpandir(!expandir);
+            }}
+            checked={expandir}
+            size="medium"
+          />
+        </Box>
+        <Box className="options-pagination">
+          {/*
 						<ButtonBase>
 							<ArrowBack />
 	    Voltar
@@ -139,10 +134,9 @@ export default function Chart() {
 	    Proxima
 	    <ArrowFoward />
     </ButtonBase>
-						 */
-					}
-				</Box>
-			</Box>
-		</Box>
-	);
+						 */}
+        </Box>
+      </Box>
+    </Box>
+  );
 }
